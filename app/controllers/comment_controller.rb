b76682603f69e0ac6af6d80394906533d3e2fc88
pombird <shun-0405-uver@outlook.jp>
2018-12-10 13:14:51 +0900

    [add] comment

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -1,4 +1,20 @@
 class CommentController < ApplicationController
+  before_action :authenticate_user!, only: [:get, :post, :put, :delete]
+
+
+  # コメントの取得
+  def get
+    comment = Comment.find(params[:id])
+
+    if comment.user_id != current_user.id then
+      render :json => { error: "コメントを取得する権限がありません" }, status: 401
+    elsif
+      render :json =>  {
+        id: comment.id,
+        comment: comment.comment,
+      }
+    end
+  end
 
   # コメント投稿
   def post
@@ -10,8 +26,27 @@ class CommentController < ApplicationController
         :article_id => params[:article_id]
       )
     end
-
-    render :json => { message: "ok" }
+    render :json => { message: "コメントを作成しました" }
   end
 
+  # コメントの更新
+  def update
+    comment = Comment.find(params[:id])
+    if comment != nil 
+      comment.update!(comment: params[:comment])
+    end
+    render :json => { message: "コメントを更新しました" }
+  end
+
+  def delete
+    comment = Comment.find(params[:id])
+
+    if comment.user_id != current_user.id then
+      render :json => { error: "コメントを削除する権限がありません" }, status: 401
+    elsif
+      comment.delete
+      render :json => { message: "コメントを削除しました" }
+    end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,16 @@ Rails.application.routes.draw do
   get  "items/show/:name/:id",  to: "article#show" 
 
   get   '/users/:name/items', to: "user#items"
+
+  get '/comments/:id', to: 'comment#get'
   
   put  "items/update/:id", to: "article#update"
+  put '/comments/:id', to: 'comment#update'
   post "items/create",    to: "article#create"
   
-  post "comments/post", to: "comment#post"
+  post "comments", to: "comment#post"
+
+  delete '/comments/:id', to: 'comment#delete'
   
   # Devise 設定
   mount_devise_token_auth_for 'User', at: 'auth',
