51dd7141e90661c442c3a20f951dd305a2ab225b
n-ayaka <kd1188211@st.kobedenshi.ac.jp>
2019-01-08 12:04:07 +0900

    [update]Article index(items,presents)

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,31 @@
 Rails.application.routes.draw do
-  
-  get  "/articles/:id",  to: "article#edit"
-
-  get  "items/show/:name/:id",  to: "article#show" 
+ 
+  # 先に設定されているものが優先される
+  #-----------------------------------
+  # 記事
+  #-----------------------------------
   get   "/articles", to: "article#index"
-
-  get   '/users/:name/items', to: "user#items"
-
+  get   "/articles/:id",  to: "article#edit"
+  get   "/articles/:name/:id",  to: "article#show"
+  put   "/articles/:id", to: "article#update"
+  post  "/articles/create", to: "article#create"
+  #----------------------------------
+  # ユーザー
+  #----------------------------------
+  get  '/users/:name/items', to: "user#items"
+  #----------------------------------
+  # コメント
+  #----------------------------------
   get '/comments/:id', to: 'comment#get'
-  
-  put  "/articles/:id", to: "article#update"
-  
-  put '/comments/:id', to: 'comment#update'
-  post "items/create",    to: "article#create"
-  
   post "comments", to: "comment#post"
-
   delete '/comments/:id', to: 'comment#delete'
-  
+  #---------------------------------
+  # お気に入り
+  #--------------------------------
+  put '/favorites/:id', to: 'favorite#update'
+  #---------------------------------
   # Devise 設定
+  #---------------------------------
   mount_devise_token_auth_for 'User', at: 'auth',
   controllers: {
     registrations: "registrations"
